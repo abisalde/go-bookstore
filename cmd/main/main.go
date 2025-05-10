1a3b34a3fb97c54cf26e1549f5864c7b13edce53
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2025-05-10 19:29:07 +0100

    finish setup

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/abisalde/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/", r)
+
+	fmt.Printf("🚀 Starting server at Port 9010\n")
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+}
